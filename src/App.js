101167ee6dab015f6f8cb10bcbe074eb5bdaf4ee
jeshkaB <y.bebeshko@ukr.net>
2022-06-26 16:26:17 +0300

    ignor .idea

--- src/App.js
+++ src/App.js
@@ -7,28 +7,27 @@ import {useEffect, useState} from "react";
 import Launches from "./components/launches";
 
 function App() {
-    let [launches, setLaunches] = useState([]);
-    useEffect(() => {
-        fetch('https://api.spacexdata.com/v3/launches/')
-            .then(value => value.json())
-            .then(value => {
-                setLaunches(value)
-            })
-    }, [])
-    // let l = launches.filter(launch=>launch.launch_year !==2020);
-    return (
-        <div className="App">
-            {launches.filter(launch => launch.launch_year !== '2020').map((launch, index) =>
+    // let [launches, setLaunches] = useState([]);
+    // useEffect(() => {
+    //     fetch('https://api.spacexdata.com/v3/launches/')
+    //         .then(value => value.json())
+    //         .then(value => {
+    //             setLaunches(value)
+    //         })
+    // }, [])
+    //   return (
+    //     <div className="App">
+    //         {launches.filter(launch => launch.launch_year !== '2020').map((launch, index) =>
                 <Launches
-                    key={index}
-                    year={launch.launch_year}
-                    name={launch.mission_name}
-                    link={launch.links.mission_patch_small}
-                />)}
-
-
-        </div>
-    );
+    //                 key={index}
+    //                 year={launch.launch_year}
+    //                 name={launch.mission_name}
+    //                 link={launch.links.mission_patch_small}
+                />
+    //
+    //
+    //     </div>
+    // );
 }
 
 export default App;
--- src/components/launches.js
+++ src/components/launches.js
@@ -1,10 +1,21 @@
-export default function Launches ({name, year, link}) {
-    return (
-        <div>
-            <h2> mission name: {name} </h2>
-            <p> launch year: {year} </p>
-            <p> mission patch small: <a href= {link} target='blank'> {link}:</a> </p>
+import {useEffect, useState} from "react";
+export default function Launches () {
+    let [launches, setLaunches] = useState([]);
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => {
+                    setLaunches(value)
+                }
+            );
+    }, [])
 
-        </div>
-    );
+    return (
+        launches.filter(launch => launch.launch_year !== '2020').map((launch) =>
+            <div>
+                <h2> mission name: {launch.name} </h2>
+                <p> launch year: {launch.launch_year} </p>
+                <p> mission patch small: <a href={launch.link.mission_patch_small} target='blank'> {launch.link.mission_patch_small}:</a></p>
+            </div>
+        ))
 }
